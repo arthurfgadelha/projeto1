d8c638820130f62f35e2da81a25b1a81479a2363
arthurfgadelha <arthurgadelha30@gmail.com>
2022-10-13 23:38:12 -0300

    configurando estrutura da tela 2

--- Screens/EmailScreen.js
+++ Screens/EmailScreen.js
@@ -18,23 +18,31 @@ export default function EmailScreen ({ route }){
 
 
     function renderItem ({item}) {
-        return <View>
-            <Text>{item.from}</Text>
+        return <View style={styles.userEmail}>
+                    <View style={styles.tituloEmail}>
+                        <Text>{item.tittle}</Text>
+                    </View>
+                    <View style={styles.infoRemetente}>
+                        <Image style={styles.imgPerfil} source={{uri: item.picture}}/>
+                        <Text>De {item.to} Para {item.from}</Text>
+                        <Text>{item.time}</Text>
+                    </View>
+                    <View style={styles.assunto}>
+                        <Text>{item.body}</Text>
+                    </View>
         </View>
     }
 
 
     return (
-    <View style={styles.container}>
+     <View style={styles.container}>
         <StatusBar style="auto"/>
-        <View style={styles.tituloEmail}>
             <FlatList
                 data={mensagem}
                 renderItem={renderItem}
                 keyExtractor={item => item.id}
                 showsVerticalScrollIndicator={false}
             />
-        </View>    
         </View>
     )
 }
@@ -45,15 +53,17 @@ const styles = StyleSheet.create({
     },
     tituloEmail: {
         height: 100,
-      
+        backgroundColor: 'red',
     },
     infoRemetente: {
         height: 60,
-        backgroundColor: 'green'
+        backgroundColor: 'green',
+        justifyContent: 'space-between',
     },
     assunto: {
         flex: 1,
-        backgroundColor: 'blue'
+        backgroundColor: 'blue',
+        alignItems: 'center',
     },
     titulo: {
         fontWeight: 'bold',
@@ -61,6 +71,12 @@ const styles = StyleSheet.create({
     userEmail: {
         height: 'auto',
         flexDirection: 'row',
+    },
+    imgPerfil: {
+        height: 50,
+        width: 50,
+        margin: 5,
+        borderRadius: 25,
     },
 
 });
